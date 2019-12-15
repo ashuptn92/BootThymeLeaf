5aeff05db60c4022c43b25b6ea14037dd88db0bd
Ashutosh Kumar <ashuptn92@outlook.com>
2019-12-15 21:01:04 +0530

    Ashutosh boot app

---
+++ SpringBootThyeLeaf/src/main/java/com/app/demo/SpringBootThyeLeafApplication.java
@@ -0,0 +1,13 @@
+package com.app.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThyeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootThyeLeafApplication.class, args);
+	}
+
+}
